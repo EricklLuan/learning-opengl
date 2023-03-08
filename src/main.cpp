26a590b0ef1a2db9615beb07d090e1b137c13ec0
Erick Luan <erick.luan.silva1308@gmail.com>
2023-03-08 18:41:56 -0300

    window with glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+static void glfwError(int id, const char* description) {
+  std::cout << description << std::endl;
+}
+
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+  glfwSetErrorCallback(&glfwError);
+  glfwInit();
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Learnig Opengl", nullptr, nullptr);
+  if (window == nullptr) {
+    std::cout << "Failed to create GLFW window\n";
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD\n";
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, &framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  
+  glfwTerminate();
 }
