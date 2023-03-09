c99c0cac1babfdaa7eddd6d9aba8b4d92d7e4698
Erick Luan <erick.luan.silva1308@gmail.com>
2023-03-09 12:56:46 -0300

    hello triangle

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
+}
+
+static void process_input(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
 }
 
 static void glfwError(int id, const char* description) {
@@ -32,10 +38,97 @@ int main(int, char**) {
   glViewport(0, 0, 800, 600);
   glfwSetFramebufferSizeCallback(window, &framebuffer_size_callback);
 
+  std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << "\n";
+
+  float verticies[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+  };
+
+  // Define como a GPU ira tratar a informação presente no array de vertices
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  // Armazena uma grande quantidade de vertices na memoria da GPU
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  // Tipo de buffer, tamanho do array, array, tipo de desenho
+  glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+  // Indice da informação, tamanho da informaçõa, tipo da informação, tamanho da informação em bites, offset
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  const char* vertexShaderSrc = 
+  "#version 140\n"
+  "attribute vec3 position;\n"
+  "void main(){\n"
+  " gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+  "}\0";
+
+  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL);
+  glCompileShader(vertexShader);  
+
+  int sucess;
+  char log[512];
+
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &sucess);
+  if (!sucess) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, log);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED:\n" << log << "\n";
+  }
+
+  const char* fragmentShaderSrc = 
+  "#version 140\n"
+  "out vec4 fragColor;\n"
+  "void main(){\n"
+  " fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+  "}\0";
+
+  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSrc, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &sucess);
+  if (!sucess) {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, log);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED:\n" << log << "\n";
+  }
+
+  unsigned int shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+
+  glBindAttribLocation(shaderProgram, 0, "position");
+  glLinkProgram(shaderProgram);
+  
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &sucess);
+  if (!sucess) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, log);
+  }
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
   while (!glfwWindowShouldClose(window)) {
-    glfwSwapBuffers(window);
+    process_input(window);
+
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+   
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
     glfwPollEvents();
+    glfwSwapBuffers(window);
   }
   
   glfwTerminate();
 }
+  
